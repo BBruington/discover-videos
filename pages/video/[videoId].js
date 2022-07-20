cfc4d13d25ee6885517887f7996b7776ce38337e
BBruington <Psychological_Chemist@hotmail.com>
2022-07-20 16:36:00 -0400

    Modal iframe structure

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,21 +1,62 @@
 import { useRouter } from 'next/router';
 import Modal from 'react-modal';
 
+import clsx from 'classnames';
+import styles from '../../styles/Video.module.css';
+
 Modal.setAppElement('#__next');
 
 const Video = () => {
 
   const router = useRouter();
 
+  const video = {
+    title: 'Hi cute dog',
+    publishTime: '1990-01-01',
+    description: 'this is a description',
+    channelTitle: 'Paramount Pictures',
+    viewCount: 222,
+  };
+
+  const {title, publishTime, description, channelTitle, viewCount} = video;
+
   return (
-    <div>Video Page {router.query.videoId}
+    <div className={styles.container}>
       <Modal 
-      isOpen={modalIsOpen}
-      onAfterOpen={afterOpenModal}
-      onRequestClose={closeModal}
-      style={customStyles}
-      contentLabel='example modal'>
-        <div>Modal body</div>
+      isOpen={true}
+      contentLabel='Watch the video'
+      onRequestClose={() => router.back()}
+      className={styles.modal}
+      overlayClassName={styles.overlay}
+      >
+          <iframe 
+          id="player" 
+          className={styles.videoPlayer}
+          type="text/html" 
+          width="100%" height="390"
+          src={`http://www.youtube.com/embed/${router.query.videoId}?enablejsapi=1&origin=http://example.com&controls=0&rel=0`}
+          frameborder="0">
+          </iframe>
+
+          <div className={styles.modalBody}>
+            <div className={styles.modalBodyContent}>
+              <div className={styles.col1}>
+                <p className={styles.publishTime}>{publishTime}</p>
+                <p className={styles.title}>{title}</p>
+                <p className={styles.description}>{description}</p>
+              </div>
+              <div className={styles.col2}>
+                <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                  <span className={styles.textColor}>Cast: </span>
+                  <span className={styles.channelTitle}>{channelTitle}</span>
+                </p>
+                <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                  <span className={styles.textColor}>View Count: </span>
+                  <span className={styles.channelTitle}>{viewCount}</span>
+                </p>
+              </div>
+            </div>
+          </div>
       </Modal>
     </div>
   )
